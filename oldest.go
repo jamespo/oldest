84e323ecfa5e92430506ca68a3bd15c4d95646ab
James Powell <jamespo@gmail.com>
2022-12-23 19:29:22 +0000

    change mode dependent on exe name

--- oldest.go
+++ oldest.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"time"
 )
 
@@ -13,23 +14,23 @@ import (
 type fn func(time.Time, time.Time) bool
 
 func myFlagUsage() {
-	fmt.Printf("Usage: %s [OPTIONS] [path to search]\n", os.Args[0])
+	fmt.Printf("Usage: %s [OPTIONS] [path to search]\n", path.Clean(os.Args[0]))
 	flag.PrintDefaults()
-	fmt.Println("Note: Search is not recursive")
+	fmt.Println("Note: Search is not recursive. Default path is CWD")
 }
 
-// getArgs - read flags to cli
+// getArgs - read & validate flags to cli
 func getArgs() (string, bool) {
 	flag.Usage = myFlagUsage
 	oldestArgPtr := flag.Bool("oldest", false, "Search for oldest")
 	newestArgPtr := flag.Bool("newest", false, "Search for newest")
 	flag.Parse()
 
-	var path string
+	var dirPath string
 
 	if flag.NArg() == 1 {
-		// path specified
-		path = flag.Args()[0]
+		// dirPath specified
+		dirPath = flag.Args()[0]
 	}
 
 	findOldest := true
@@ -38,35 +39,37 @@ func getArgs() (string, bool) {
 		findOldest = true
 	} else if *newestArgPtr {
 		findOldest = false
+	} else if path.Clean(os.Args[0]) == "newest" {
+		// if called as newest, search for newest
+		findOldest = false
 	}
 
-	// validate path - default to CWD if empty
+	// validate dirPath - default to CWD if empty
 	var err error
-	if path == "" {
-		path, err = os.Getwd()
+	if dirPath == "" {
+		dirPath, err = os.Getwd()
 		if err != nil {
-			_ = path // get around path not used
 			log.Fatal(err)
 		}
 	} else {
-		if !isDirectory(path) {
+		if !isDirectory(dirPath) {
 			log.Fatal("Not a directory")
 		}
 	}
 
-	return path, findOldest
+	return dirPath, findOldest
 }
 
 func main() {
-	path, findOldest := getArgs()
+	dirPath, findOldest := getArgs()
 
 	var fileResult string
 	var fileResultErr error
 
 	if findOldest {
-		fileResult, fileResultErr = getOldest(path)
+		fileResult, fileResultErr = getOldest(dirPath)
 	} else {
-		fileResult, fileResultErr = getNewest(path)
+		fileResult, fileResultErr = getNewest(dirPath)
 	}
 
 	if fileResultErr != nil {
@@ -123,6 +126,10 @@ func findFile(path string, comparator fn) (string, error) {
 		if !file.IsDir() {
 			fileInfo, _ := file.Info()
 			fileModTime := fileInfo.ModTime()
+			if fileInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
+				// file is symlink, skip
+				continue
+			}
 			if unset || comparator(fileModTime, oldestOrNewestFileTime) {
 				oldestOrNewestFile = file.Name()
 				oldestOrNewestFileTime = fileModTime
